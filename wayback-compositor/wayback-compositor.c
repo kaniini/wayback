50dc37cd9ff4d2c1185ea06a9a3ea7fc8f16613d
axtloss <rose@pinkro.se>
2025-07-06 20:30:47 +0200

    Split wayback in xorg like structure
    Splits the tree into three components, wayback-compositor, xwayback and wayback-session

---
+++ wayback-compositor/wayback-compositor.c
@@ -0,0 +1,14 @@
+/*
+ * Wayback is essentially the tinywl compositor included in wlroots
+ * but stripped down to only support an X session.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+	printf("wayback-compositor\n");
+	return 0;
+}
