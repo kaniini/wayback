432df2127f55a30c2797bc3e14a74a39c693a09f
axtloss <rose@pinkro.se>
2025-07-06 20:30:47 +0200

    Split wayback in xorg like structure
    Splits the tree into three components, wayback-compositor, xwayback and wayback-session

---
+++ wayback-session/wayback-session.c
@@ -0,0 +1,14 @@
+/*
+ * wayback-session launches the wayback compositor, XWayback/XWayland
+ * and the given session executable
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("wayback-session \n");
+	return 0;
+}
---
+++ xwayback/xwayback.c
@@ -0,0 +1,15 @@
+/*
+ * XWayback is a wrapper around wayback-compositor
+ * and XWayland, it launches both in succession and
+ * functions similiar to a traditional X server.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("XWayback\n");
+	return 0;
+}
